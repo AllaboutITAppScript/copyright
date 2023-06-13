f2680cf2966aa8bd2853ed5252b494d389a049bf
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2023-06-13 09:57:01 +0700

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,1 +1,12 @@
+    $('#btn').hide()
+  const owner = "Copyright  <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='cc'> Teerapong Promwang"
 
+/** ตั้งค่าเริ่มต้น และโหลดพิกัดตำแหน่งที่อยู่ **/
+  $(document ).ready(async function() {
+      document.getElementById('message').innerHTML= owner;
+      document.getElementById('message').className = 'msgBg';
+      document.getElementById('loader').style.display = "none";
+     // const location =  await getlocation();
+      //console.log(location)
+        // $('#canvas').show()
+  });
