80d3298e6da2643b202f9b056ac8e4472199d671
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2023-06-13 11:38:22 +0700

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,5 +1,5 @@
     $('#btn').hide()
-  const cr = "Copyright  <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='cc'> Teerapong Promwang"
+  const cr = "พัฒนาโดย:นายธีรพงศ์ พรหมวัง <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='ครบเครื่อง เรื่องไอที'>"
   $(document ).ready(async function() {
       document.getElementById('message').innerHTML= cr;
       document.getElementById('message').className = 'msgBg';
