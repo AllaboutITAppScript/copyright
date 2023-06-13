53787109decb216d4c79bea74f076e18d680d0f9
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2023-06-13 10:13:31 +0700

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,9 +1,7 @@
     $('#btn').hide()
-  const owner = "Copyright  <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='cc'> Teerapong Promwang"
-
-/** ตั้งค่าเริ่มต้น และโหลดพิกัดตำแหน่งที่อยู่ **/
+  const cr = "Copyright  <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='cc'> Teerapong Promwang"
   $(document ).ready(async function() {
-      document.getElementById('message').innerHTML= owner;
+      document.getElementById('message').innerHTML= cr;
       document.getElementById('message').className = 'msgBg';
       document.getElementById('loader').style.display = "none";
      // const location =  await getlocation();
