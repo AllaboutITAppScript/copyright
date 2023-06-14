8c26006bfbec2cd7814e8f88953cbd0b7a0442a7
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2023-06-14 09:03:53 +0700

    Update credit.js

--- credit.js
+++ credit.js
@@ -1,4 +1,5 @@
- window.addEventListener('load', () => {
+
+  window.addEventListener('load', () => {
     let text = document.getElementById("text").innerHTML = `<div id="loader">
     <div class="overlay">
       <div class="overlay__inner">
@@ -8,4 +9,8 @@
       </div>
     </div>
   </div>`;
+   const cr = `พัฒนาโดย:นายธีรพงศ์ พรหมวัง <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='ครบเครื่อง เรื่องไอที'>`
+   document.getElementById('message').innerHTML= cr;
+   document.getElementById('message').className = 'msgBg';
+   document.getElementById('loader').style.display = "none";
   });
