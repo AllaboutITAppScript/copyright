b6297e2511ab305eeda9795dfa87b1a87e86273c
AllaboutITAppScript <121791279+AllaboutITAppScript@users.noreply.github.com>
2023-06-14 09:00:01 +0700

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,4 +1,4 @@
-     const cr = "พัฒนาโดย:นายธีรพงศ์ พรหมวัง <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='ครบเครื่อง เรื่องไอที'>"
+     const cr = `พัฒนาโดย:นายธีรพงศ์ พรหมวัง <img src='https://i.im.ge/2022/12/18/dnNRqa.2564-01-03-094436.png' width='25' height='25' alt='ครบเครื่อง เรื่องไอที'>`
       document.getElementById('message').innerHTML= cr;
       document.getElementById('message').className = 'msgBg';
       document.getElementById('loader').style.display = "none";
